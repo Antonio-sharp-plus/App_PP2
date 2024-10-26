e2565a6577591271673f6ee2d4469ed0316c4bca
Juan Pablo Schettini <juanpablo.schettini2@gmail.com>
2024-10-26 17:31:58 -0300

    Up-Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,15 @@
-
+//Detalles de Firebase
 
 export const environment = {
   production: false,
-
+  firebaseConfig:  {
+    apiKey: "AIzaSyDHKhMWDcS4T3g6dFXhMiJsKThoeC14iNQ",
+    authDomain: "listapp-585fb.firebaseapp.com",
+    projectId: "listapp-585fb",
+    storageBucket: "listapp-585fb.appspot.com",
+    messagingSenderId: "676863439546",
+    appId: "1:676863439546:web:91cdabf09b44fb3edac883",
+    measurementId: "G-KC98KP4CDB"
+  }
 };
 
